b81a056d348fa0bf332dcd8bf847c8500e87cdef
Prakash <141093prakash@gmail.com>
2021-03-09 18:48:16 -0500

    my code

---
+++ src/main/java/Homeapp.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Homeapp {
+
+    List<String> names = new ArrayList<>();
+
+
+    public boolean addNames(String name){
+        return names.add(name);
+    }
+    public boolean removeName(String name){
+        return names.remove(name);
+    }
+    public List<String> getNames(){
+        return names;
+    }
+
+}
---
+++ src/test/java/HomeTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.*;
+
+public class HomeTest {
+
+
+            Homeapp app;
+            @BeforeSuite
+            public void setUp(){
+                System.out.println("Test Suite");
+
+
+
+    }
+
+}
